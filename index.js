ae4a15e89242a057a0e6a7ac417c392f2e1f6c91
Aral Balkan <aral@ind.ie>
2019-02-25 13:30:49 +0000

    Actually serve the requested directory & add manual port selection

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ const os = require('os')
 
 const arguments = process.argv
 
-if (arguments.length > 3) {
-  console.log('Usage: https-server [folder-to-serve]')
+if (arguments.length > 4) {
+  console.log('Usage: https-server [folder-to-serve (default=.)] [port (default=443)]')
   process.exit()
 }
 
 // If no path is passed, serve the current folder.
 // If there is a path, serve that.
 let pathToServe = '.'
-if (arguments.length === 3) {
+if (arguments.length >= 3) {
   pathToServe = arguments[2]
+}
+
+let port = 443
+// If a port is given, use that.
+if (arguments.length === 4) {
+  port = parseInt(arguments[3])
 }
 
 if (!fs.existsSync(pathToServe)) {
@@ -41,14 +47,19 @@ if (!fs.existsSync(nodecertDirectory)) {
 const app = express()
 
 app.use(morgan('tiny'))
-app.use(express.static('dist'))
+app.use(express.static(pathToServe))
 
 try {
   const server = https.createServer({
     key: fs.readFileSync(path.join(nodecertDirectory, 'localhost-key.pem')),
     cert: fs.readFileSync(path.join(nodecertDirectory, 'localhost.pem'))
-  }, app).listen(443, () => {
-    console.log(`\n 🎉 Serving ${pathToServe} on https://localhost\n`)
+  }, app).listen(port, () => {
+    const serverPort = server.address().port
+    let portSuffix = ''
+    if (serverPort !== 443) {
+      portSuffix = `:${serverPort}`
+    }
+    console.log(`\n 🎉 Serving ${pathToServe} on https://localhost${portSuffix}\n`)
   })
 } catch (error) {
   console.log('\nError: could not start server', error)
