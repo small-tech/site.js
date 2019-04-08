4d3af1a47c6966ab153c18d3ff608e93bedc91df
Aral Balkan <mail@ar.al>
2019-04-08 10:45:43 +0800

    Copy node_modules to external location on first live run

--- bin/web-server.js
+++ bin/web-server.js
@@ -9,7 +9,11 @@ const pm2 = require('pm2')
 const childProcess = require('child_process')
 const arguments = require('minimist')(process.argv.slice(2), {boolean: true})
 
-const pm2Path = path.join(__dirname, '../node_modules/pm2/bin/pm2')
+const copy = require('recursive-copy')
+
+const internalNodeModulesDirectory = path.join(__dirname, '..', 'node_modules')
+const externalNodeModulesDirectory = path.join(os.homedir(), '.indie-web-server', 'node_modules')
+const pm2Path = path.join(externalNodeModulesDirectory, 'pm2', 'bin', 'pm2')
 
 if (arguments._.length > 2 || arguments.help === true) {
 
@@ -57,113 +61,145 @@ if (arguments.version !== undefined) {
   process.exit()
 }
 
+// Ensure that we have copied PM2 to a well-known, external location
+// so it can be called when Indie Web Server is executed as a standalone
+// binary wrapped with nexe.
+function ensurePM2 (callback) {
+  if (!fs.existsSync(externalNodeModulesDirectory)) {
+    console.log(' 👷 First run: copying node modules.')
+    try {
+      fs.mkdirSync(externalNodeModulesDirectory, {recursive: true})
+    } catch (error) {
+      console.log('\n 🔥 Error: could not create external node modules directory', error)
+      throw error
+    }
+    copy(internalNodeModulesDirectory, externalNodeModulesDirectory, (error, results) => {
+      if (error) {
+        console.log('\n 🔥 Error: could not copy node modules', error)
+        throw error
+      } else {
+        console.log(' 💅 External node modules ready.')
+        callback()
+      }
+    })
+  } else {
+    console.log(' 💅 External node modules ready.')
+    callback()
+  }
+}
+
 // Monitor (pm2 proxy).
 if (arguments.monitor !== undefined) {
-  // Launch pm2 monit.
-  const options = {
-    env: process.env,
-    stdio: 'inherit'  // Display output.
-  }
-
-  try {
-    childProcess.execSync(`sudo ${pm2Path} monit`, options)
-  } catch (error) {
-    console.log(`\n 👿 Failed to launch the process monitor.\n`)
-    process.exit(1)
-  }
-  process.exit(0)
+  ensurePM2(() => {
+    const options = {
+      env: process.env,
+      stdio: 'inherit'  // Display output.
+    }
+    // Launch pm2 monit.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} monit`, options)
+    } catch (error) {
+      console.log(`\n 👿 Failed to launch the process monitor.\n`)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 }
 
 // Logs (pm2 proxy).
 if (arguments.logs !== undefined) {
-  // Launch pm2 logs.
-  const options = {
-    env: process.env,
-    stdio: 'inherit'  // Display output.
-  }
-
-  try {
-    childProcess.execSync(`sudo ${pm2Path} logs web-server`, options)
-  } catch (error) {
-    console.log(`\n 👿 Failed to get the logs.\n`)
-    process.exit(1)
-  }
-  process.exit(0)
+  ensurePM2(() => {
+    const options = {
+      env: process.env,
+      stdio: 'inherit'  // Display output.
+    }
+    // Launch pm2 logs.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} logs web-server`, options)
+    } catch (error) {
+      console.log(`\n 👿 Failed to get the logs.\n`)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 }
 
 // Info (pm2 proxy).
 if (arguments.info !== undefined) {
-  // Launch pm2 logs.
-  const options = {
-    env: process.env,
-    stdio: 'inherit'  // Display output.
-  }
-
-  try {
-    childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
-  } catch (error) {
-    console.log(`\n 👿 Failed to show detailed information on the web server.\n`)
-    process.exit(1)
-  }
-  process.exit(0)
+  ensurePM2(() => {
+    const options = {
+      env: process.env,
+      stdio: 'inherit'  // Display output.
+    }
+    // Launch pm2 logs.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
+    } catch (error) {
+      console.log(`\n 👿 Failed to show detailed information on the web server.\n`)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 }
 
 // Offline (pm2 proxy for unstartup + delete)
 if (arguments.offline !== undefined) {
-  const options = {
-    env: process.env,
-    stdio: 'pipe'   // Suppress output.
-  }
-
-  // Do some cleanup, display a success message and exit.
-  function success () {
-    // Try to reset permissions on pm2 so that future uses of pm2 proxies via web-server
-    // in this session will not require sudo.
-    try {
-      childProcess.execSync('sudo chown $(whoami):$(whoami) /home/$(whoami)/.pm2/rpc.sock /home/$(whoami)/.pm2/pub.sock', options)
-    } catch (error) {
-      console.log(`\n 👿 Warning: could not reset permissions on pm2.`)
+  ensurePM2(() => {
+    const options = {
+      env: process.env,
+      stdio: 'pipe'   // Suppress output.
     }
 
-    // All’s good.
-    console.log(`\n 😈 Server is offline and removed from startup items.\n`)
-    process.exit(0)
-  }
+    // Do some cleanup, display a success message and exit.
+    function success () {
+      // Try to reset permissions on pm2 so that future uses of pm2 proxies via web-server
+      // in this session will not require sudo.
+      try {
+        childProcess.execSync('sudo chown $(whoami):$(whoami) /home/$(whoami)/.pm2/rpc.sock /home/$(whoami)/.pm2/pub.sock', options)
+      } catch (error) {
+        console.log(`\n 👿 Warning: could not reset permissions on pm2.`)
+      }
 
-  // Is the server running?
-  try {
-    childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
-  } catch (error) {
-    console.log(`\n 👿 Server is not running as a live daemon; nothing to take offline.\n`)
-    process.exit(1)
-  }
+      // All’s good.
+      console.log(`\n 😈 Server is offline and removed from startup items.\n`)
+      process.exit(0)
+    }
 
-  // Try to remove from startup items.
-  try {
-    childProcess.execSync(`sudo ${pm2Path} unstartup`, options)
-  } catch (error) {
-    console.log(`\n 👿 Could not remove the server from startup items.\n`)
-    process.exit(1)
-  }
+    // Is the server running?
+    try {
+      childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
+    } catch (error) {
+      console.log(`\n 👿 Server is not running as a live daemon; nothing to take offline.\n`)
+      process.exit(1)
+    }
 
-  // If the server was started as a startup item, unstartup will also
-  // kill the process. Check again to see if the server is running.
-  try {
-    childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
-  } catch (error) {
+    // Try to remove from startup items.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} unstartup`, options)
+    } catch (error) {
+      console.log(`\n 👿 Could not remove the server from startup items.\n`)
+      process.exit(1)
+    }
+
+    // If the server was started as a startup item, unstartup will also
+    // kill the process. Check again to see if the server is running.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} show web-server`, options)
+    } catch (error) {
+      success()
+    }
+
+    // The server is still on (it was not started as a startup item). Use
+    // pm2 delete to remove it.
+    try {
+      childProcess.execSync(`sudo ${pm2Path} delete web-server`, options)
+    } catch (error) {
+      console.log(`\n 👿 Could not delete the server daemon.\n`)
+      process.exit(1)
+    }
+
     success()
-  }
-
-  // The server is still on (it was not started as a startup item). Use
-  // pm2 delete to remove it.
-  try {
-    childProcess.execSync(`sudo ${pm2Path} delete web-server`, options)
-  } catch (error) {
-    console.log(`\n 👿 Could not delete the server daemon.\n`)
-    process.exit(1)
-  }
-
-  success()
+  })
 }
 
 // If no path is passed, serve the current folder.
@@ -193,46 +229,48 @@ if (!fs.existsSync(pathToServe)) {
 // If live mode is specified, run as a daemon using the pm2 process manager.
 // Otherwise, start it as a regular process.
 if (arguments.live !== undefined) {
-
-  pm2.connect((error) => {
-    if (error) {
-      console.log(error)
-      process.exit(1)
-    }
-
-    pm2.start({
-      script: path.join(__dirname, 'daemon.js'),
-      args: pathToServe,
-      name: 'web-server',
-      autorestart: true
-    }, (error, processObj) => {
+  ensurePM2(() => {
+    pm2.connect((error) => {
       if (error) {
-        throw error
-      }
-
-      console.log(`${webServer.version()}\n 😈 Launched as daemon on https://${os.hostname()}\n`)
-
-      //
-      // Run the script that tells the process manager to add the server to launch at startup
-      // as a separate process with sudo privileges.
-      //
-      const options = {
-        env: process.env,
-        stdio: 'pipe'     // Suppress output.
-      }
-
-      try {
-        const output = childProcess.execSync(`sudo ${path.join(__dirname, '../node_modules/pm2/bin/pm2')} startup`, options)
-      } catch (error) {
-        console.log(` 👿 Failed to add server for auto-launch at startup.\n`)
-        pm2.disconnect()
+        console.log(error)
         process.exit(1)
       }
 
-      console.log(` 😈 Installed for auto-launch at startup.\n`)
+      pm2.start({
+        script: path.join(__dirname, 'daemon.js'),
+        args: pathToServe,
+        name: 'web-server',
+        autorestart: true
+      }, (error, processObj) => {
+        if (error) {
+          throw error
+        }
 
-      // Disconnect from the pm2 daemon. This will also exit the script.
-      pm2.disconnect()
+        console.log(`${webServer.version()}\n 😈 Launched as daemon on https://${os.hostname()}\n`)
+
+        //
+        // Run the script that tells the process manager to add the server to launch at startup
+        // as a separate process with sudo privileges.
+        //
+        const options = {
+          env: process.env,
+          stdio: 'pipe'     // Suppress output.
+        }
+
+        try {
+          const output = childProcess.execSync(`sudo ${pm2Path} startup`, options)
+        } catch (error) {
+          console.log(` 👿 Failed to add server for auto-launch at startup.\n`)
+          console.log(error)
+          pm2.disconnect()
+          process.exit(1)
+        }
+
+        console.log(` 😈 Installed for auto-launch at startup.\n`)
+
+        // Disconnect from the pm2 daemon. This will also exit the script.
+        pm2.disconnect()
+      })
     })
   })
 } else {
