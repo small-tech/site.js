54dd4b61dc986c13806defb2291713a729cb3786
Aral Balkan <mail@ar.al>
2019-04-09 20:33:55 +0800

    Zip up the source and clean up in the build script

--- bin/build.js
+++ bin/build.js
@@ -25,6 +25,14 @@ fs.mkdirSync(macOSVersionPath, {recursive: true})
 
 async function build () {
   //
+  // Zip the source.
+  //
+  console.log('   • Zipping up the source for inclusion in the binary…')
+
+  const mainSourceDirectory = path.join(__dirname, '..')
+  childProcess.execSync(String.raw`rm -f web-server.zip && zip web-server.zip * -x \*.git\* \*dist-iws\* -r`, {env: process.env, cwd: mainSourceDirectory})
+
+  //
   // Build.
   //
   console.log('   • Building Linux version…')
@@ -49,14 +57,25 @@ async function build () {
   console.log('   • Zipping binaries…')
 
   const zipFileName = `${version}.zip`
-  const linuxVersionWorkingDirectory = path.join(__dirname, '..', linuxVersionPath)
-  const macOSVersionWorkingDirectory = path.join(__dirname, '..', macOSVersionPath)
+  const linuxVersionWorkingDirectory = path.join(mainSourceDirectory, linuxVersionPath)
+  const macOSVersionWorkingDirectory = path.join(mainSourceDirectory, macOSVersionPath)
 
   childProcess.execSync(`zip ${zipFileName} web-server`, {env: process.env, cwd: linuxVersionWorkingDirectory})
   childProcess.execSync(`zip ${zipFileName} web-server`, {env: process.env, cwd: macOSVersionWorkingDirectory})
 
   //
   // Copy to web site.
+  //
+  // Note: this requires a relative directory setup that matches what I have on my
+  // ===== development machine (remember we are running in web-server/bin/):
+  //
+  // |
+  // |- site                                <- Ind.ie Web Site source
+  //     |_www/content/web-server/
+  // |- hypha
+  //     |_ web-server                      <- This project
+  //
+  // If it cannot find the Ind.ie Web Site, the build script will just skip this step.
   //
   const pathToWebServerSectionOfSite = path.join(__dirname, '../../../site/www/content/web-server/')
 
@@ -77,6 +96,10 @@ async function build () {
   } else {
     console.log('   • Skipped copy of binaries to Indie Web Site as could not find the local working copy.')
   }
+
+  console.log('   • Cleaning up…')
+
+  childProcess.execSync('rm -f web-server.zip', {env: process.env, cwd: mainSourceDirectory})
 
   console.log('\n 😁👍 Done!\n')
 }
