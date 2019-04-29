b6afba2b8808f694c5edbeee32dc8bdfcb213b31
Aral Balkan <mail@ar.al>
2019-04-29 12:55:00 +0200

    Pull out command: disable

---
+++ bin/commands/disable.js
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Command: disable
+//
+// Disables the web server daemon (stops it and removes it
+// from startup items).
+//
+//////////////////////////////////////////////////////////////////////
+
+const childProcess = require('child_process')
+const ensure = require('../utilities/ensure')
+
+ensure.systemctl()
+ensure.root('disable')
+
+try {
+  childProcess.execSync('sudo systemctl disable web-server', {env: process.env, stdio: 'pipe'})
+  childProcess.execSync('sudo systemctl stop web-server', {env: process.env, stdio: 'pipe'})
+  console.log('\n 🎈 Server stopped and removed from startup.\n')
+} catch (error) {
+  console.error(`\n 👿 Error: Could not disable web server.\n ${error}`)
+  process.exit(1)
+}
--- bin/web-server.js
+++ bin/web-server.js
@@ -111,18 +111,9 @@ switch (true) {
     require('./commands/status')
   break
 
-  // Off (turn off the server daemon and remove it from startup items).
+  // Disable (stop the server daemon and remove it from startup items).
   case command.isDisable:
-    ensure.systemctl()
-    ensure.root('disable')
-    try {
-      childProcess.execSync('sudo systemctl disable web-server', {env: process.env, stdio: 'pipe'})
-      childProcess.execSync('sudo systemctl stop web-server', {env: process.env, stdio: 'pipe'})
-      console.log('\n 🎈 Server stopped and removed from startup.\n')
-    } catch (error) {
-      console.error(`\n 👿 Error: Could not disable web server.\n ${error}`)
-      process.exit(1)
-    }
+    require('./commands/disable')
   break
 
   // Default: start the server.
