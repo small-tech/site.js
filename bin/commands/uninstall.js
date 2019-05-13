e37e422bbd544ba612c1f4e0649731ed73333b3a
Aral Balkan <mail@ar.al>
2019-05-13 21:26:32 +0100

    Implement uninstall logic

--- bin/commands/uninstall.js
+++ bin/commands/uninstall.js
@@ -9,12 +9,14 @@
 const os = require('os')
 const fs = require('fs')
 const path = require('path')
+const childProcess = require('child_process')
 
 const prompts = require('prompts')
-const Graceful = require('node-graceful')
 const actualStringLength = require('string-length')
 
+const ensure = require('../lib/ensure')
 const status = require('../lib/status')
+const disableServer = require('../lib/disable')
 
 const webServer = require('../../index')
 const clr = require('../../lib/clr')
@@ -58,28 +60,34 @@ class WarningBox {
 async function uninstall (options) {
   console.log(webServer.version())
 
-  const { isActive, isEnabled } = status()
+  ensure.systemctl()
+  ensure.root('uninstall')
+
+  const { serverIsActive, serverIsEnabled } = status()
 
   const warning = new WarningBox()
   warning.line(`${clr('WARNING!', 'yellow')} ${clr('About to uninstall Indie Web Server.', 'green')}`)
 
   // Check if the server is active/enabled and add a note about that to the warning box.
-  if (isActive && isEnabled) {
+  if (serverIsActive && serverIsEnabled) {
     warning.emptyLine()
     warning.line(`• ${clr('The server is active and enabled.', 'yellow')}`)
     warning.line('  It will be stopped and disabled.')
-  } else if (isActive) {
+  } else if (serverIsActive) {
     warning.emptyLine()
     warning.line(`• ${clr('The server is active.', 'yellow')}`)
     warning.line('  It will be stopped.')
-  } else if (isEnabled) {
+  } else if (serverIsEnabled) {
     warning.emptyLine()
     warning.line(`• ${clr('The server is enabled.', 'yellow')}`)
     warning.line('  It will be disabled.')
   }
 
+  const acmeTLSFilePath = path.join(os.homedir(), '.acme-tls')
+  const acmeTLSCertificatesExist = fs.existsSync(acmeTLSFilePath)
+
   // Check if we have provisioned TLS certificates and add a note about that to the warning box.
-  if (fs.existsSync(path.join(os.homedir(), '.acme-tls'))) {
+  if (acmeTLSCertificatesExist) {
     warning.emptyLine()
     warning.line(`• ${clr('You have provisioned Let’s Encrypt TLS certificates.', 'yellow')}`)
     warning.line('  These will be deleted.')
@@ -98,10 +106,55 @@ async function uninstall (options) {
 
   if (!response.confirmed) {
     console.log('\n ❌ Aborting…\n')
-    Graceful.exit()
+    process.exit(1)
   } else {
     console.log('\n 👋 Uninstalling…\n')
-    // TODO
+
+    // Disable the server, if it is enabled.
+    if (serverIsEnabled) {
+      try {
+        disableServer()
+        console.log(' ✔ Server disabled.\n')
+      } catch (error) {
+        process.exit(1)
+      }
+    }
+
+    // Remove the Let’s Encrypt certificates folder, if one exists.
+    if (acmeTLSCertificatesExist) {
+      try {
+        childProcess.execSync(`rm -rf ${acmeTLSFilePath}`, {env: process.env})
+        console.log(' ✔ Let’s Encrypt certificates removed.\n')
+      } catch (error) {
+        console.log(`\n ❌ Could not remove the Let’s Encrypt certificates folder (${error}).\n`)
+        process.exit(1)
+      }
+    }
+
+    // Remove the local certificates folder, if one exists.
+    const nodecertFilePath = path.join(os.homedir(), '.nodecert')
+    if(fs.existsSync(nodecertFilePath)) {
+      try {
+        childProcess.execSync(`rm -rf ${nodecertFilePath}`, {env: process.env})
+        console.log(' ✔ Local certificates removed.\n')
+      } catch (error) {
+        console.log(`\n ❌ Could not remove the local certificates folder (${error}).\n`)
+        process.exit(1)
+      }
+    }
+
+    // Remove the Indie Web Server binary itself.
+    try {
+      childProcess.execSync('rm /usr/local/bin/web-server', {env: process.env})
+      console.log(' ✔ Indie Web Server binary removed.\n')
+    } catch (error) {
+      console.log(`\n ❌ Could not remove the Indie Web Server binary (${error}).\n`)
+      process.exit(1)
+    }
+
+    console.log(`\n 🎉 Indie Web Server uninstalled.\n`)
+    console.log('\n💖 Goodbye!\n')
+    process.exit()
   }
 }
 
