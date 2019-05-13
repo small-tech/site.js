ab7f902fee4865179dd11bde9e90814501955759
Aral Balkan <mail@ar.al>
2019-05-13 20:55:45 +0100

    Tweak disable command so systemctl check happens before status check

--- bin/commands/disable.js
+++ bin/commands/disable.js
@@ -7,12 +7,15 @@
 //
 //////////////////////////////////////////////////////////////////////
 
+const fs = require('fs')
 const childProcess = require('child_process')
 
 const status = require('../lib/status')
 const ensure = require('../lib/ensure')
 
 function disable () {
+
+  ensure.systemctl()
 
   const { isActive, isEnabled } = status()
 
@@ -21,20 +24,21 @@ function disable () {
     process.exit(1)
   }
 
-  ensure.systemctl()
   ensure.root('disable')
 
   try {
     // Disable and stop the web server.
     childProcess.execSync('sudo systemctl disable web-server', {env: process.env, stdio: 'pipe'})
     childProcess.execSync('sudo systemctl stop web-server', {env: process.env, stdio: 'pipe'})
-
-    // And remove the systemd service file we created.
-    // fs.unlinkSync('/etc/systemd/system/web-server.service')
-
+    try {
+      // And remove the systemd service file we created.
+      fs.unlinkSync('/etc/systemd/system/web-server.service')
+    } catch (error) {
+      console.log(`\n 👿 Error: Web server disabled but could not delete the systemd service file (${error}).`)
+    }
     console.log('\n 🎈 Server stopped and removed from startup.\n')
   } catch (error) {
-    console.error(`\n 👿 Error: Could not disable web server.\n ${error}`)
+    console.error(`\n 👿 Error: Could not disable web server (${error}).`)
     process.exit(1)
   }
 }
