134d7748c7d2501ca5486f1f6d4785c13554d35e
Aral Balkan <mail@ar.al>
2019-06-09 16:54:20 +0100

    Update logs command

--- bin/commands/logs.js
+++ bin/commands/logs.js
@@ -2,7 +2,7 @@
 //
 // Command: logs
 //
-// Displays the web server daemon logs.
+// Displays the Site.js server daemon logs.
 //
 // Proxies: journalctl --follow --unit web-server
 //
@@ -14,7 +14,7 @@ const ensure = require('../lib/ensure')
 function logs () {
   ensure.journalctl()
   console.log(`\n 📜 Tailing logs (press Ctrl+C to exit).\n`)
-  childProcess.spawn('journalctl', ['--follow', '--unit', 'web-server'], {env: process.env, stdio: 'inherit'})
+  childProcess.spawn('journalctl', ['--follow', '--unit', 'site.js'], {env: process.env, stdio: 'inherit'})
 }
 
 module.exports = logs
