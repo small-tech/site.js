74eb9867241522f37847110833e4c6e8dc2e6eaa
Aral Balkan <mail@ar.al>
2019-06-09 17:12:36 +0100

    Remove outdated comment in lib/runtime

--- bin/lib/runtime.js
+++ bin/lib/runtime.js
@@ -1,11 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 // When run as a regular Node script, the source directory is our parent
-// directory (web-server.js resides in the <sourceDirectory>/bin directory).
-// However, when run as a standalone executable using Nexe, we currently have
-// to bundle the source code in the executable and copy it from the virtual
-// filesystem of the binary to the external file system in order to run the
-// pm2 process manager using execSync.
+// directory (site.js resides in the <sourceDirectory>/bin directory).
 //
 // For more information, please see the following issues in the Nexe repo:
 //
@@ -16,5 +12,5 @@
 
 module.exports = {
   isNode: process.argv0 === 'node',
-  isBinary: process.argv0 === 'web-server'
+  isBinary: process.argv0 === 'site'
 }
