84d125e06d4e9a19be9bd8aa1addbc11ad483ada
Aral Balkan <mail@ar.al>
2019-06-12 14:21:58 +0100

    Improve display of cows example

--- test/site/.dynamic/cows.js
+++ test/site/.dynamic/cows.js
@@ -6,7 +6,7 @@ function paveTheCowPaths(request, response, next) {
   const randomCowIndex = Math.round(Math.random()*cows.length)-1
   const randomCow = cows[randomCowIndex]
 
-  response.end(`<pre>${randomCow}</pre>`)
+  response.end(`<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error 404: Not found</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><pre style="font-size: 24px;">${randomCow}</pre></main></body></html>`)
 }
 
 module.exports = paveTheCowPaths
