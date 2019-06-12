638538267736e6d1158ffef85e8e22a7bd3b0310
Aral Balkan <mail@ar.al>
2019-06-12 14:27:59 +0100

    Improve view of server statistics example

--- test/site/.dynamic/server-stats.js
+++ test/site/.dynamic/server-stats.js
@@ -6,7 +6,8 @@ function serverStats (request, response, next) {
 
   const freeMemory = `<p>${os.freemem()} bytes</p>`
 
-  const page = `<html><head><title>Server statistics</title><style>body {font-family: sans-serif;}</style></head><body><h1>Server statistics</h1><h2>Load averages</h2>${loadAverages}<h2>Free memory</h2>${freeMemory}</body></html>`
+
+  const page = `<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Server statistics</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main style="font-size: 1.5rem;"><h1>Server statistics</h1><h2>Load averages</h2>${loadAverages}<h2>Free memory</h2>${freeMemory}</main></body></html>`
 
   response.end(page)
 }
