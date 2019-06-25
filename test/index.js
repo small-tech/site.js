48edc4405aa0785e5462744bd5b709c4e599bf30
Aral Balkan <mail@ar.al>
2019-06-25 08:15:37 +0100

    Update unit tests for Site class (failing) and changelog

--- test/index.js
+++ test/index.js
@@ -1,6 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Site.js
+//
+// Unit tests: Site.
+//
+// Copyright ⓒ 2019 Aral Balkan. Licensed under AGPLv3 or later.
+// Shared with ♥ by the Small Technology Foundation.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 const test = require('tape')
 
-const site = require('../index.js')
+const Site = require('../index.js')
 const https = require('https')
 
 const fs = require('fs')
@@ -30,7 +41,7 @@ async function secureGet (url) {
 
 test('[site.js] createServer method', t => {
   t.plan(2)
-  const server = site.createServer()
+  const server = new Site().createServer()
   t.ok(server instanceof https.Server, 'is https.Server')
 
   server.listen(443, () => {
@@ -75,43 +86,40 @@ test('[site.js] archival cascade', t => {
   const unique2URL = `https://localhost/unique-2.html`
   const overrideURL = `https://localhost/override.html`
 
-  const server = site.serve({
-    path: 'test/site',
-    callback: async () => {
-      let responseIndex, responseUnique1, responseUnique2, responseOverride
-      try {
-        responseIndex = await secureGet(indexURL)
-        responseUnique1 = await secureGet(unique1URL)
-        responseUnique2 = await secureGet(unique2URL)
-        responseOverride = await secureGet(overrideURL)
-      } catch (error) {
-        console.log(error)
-        process.exit(1)
-      }
-
-      t.equal(responseIndex.statusCode, 200, 'requestIndex succeeds')
-      t.equal(responseUnique1.statusCode, 200, 'requestUnique1 succeeds')
-      t.equal(responseUnique2.statusCode, 200, 'requestUnique2 succeeds')
-      t.equal(responseOverride.statusCode, 200, 'requestOverride succeeds')
-
-      t.equal(responseIndex.body.replace(/\s/g, ''), fs.readFileSync(path.join(__dirname, 'site', 'index.html'), 'utf-8').replace(/\s/g, ''), 'site index overrides archive indices')
-      t.equal(responseUnique1.body, archive1UniqueContent, 'archive 1 unique content loads')
-      t.equal(responseUnique2.body, archive2UniqueContent, 'archive 2 unique content loads')
-      t.equal(responseOverride.body, archive2OverrideContent, 'archive 2 content overrides archive 1 content')
-
-      server.close()
-      t.end()
-
-      // Clean up
-      fs.unlinkSync(archive1Index)
-      fs.unlinkSync(archive1Unique)
-      fs.unlinkSync(archive1Override)
-      fs.unlinkSync(archive2Index)
-      fs.unlinkSync(archive2Unique)
-      fs.unlinkSync(archive2Override)
-      fs.rmdirSync(archive1)
-      fs.rmdirSync(archive2)
+  const server = new Site({path: 'test/site'}).serve(async () => {
+    let responseIndex, responseUnique1, responseUnique2, responseOverride
+    try {
+      responseIndex = await secureGet(indexURL)
+      responseUnique1 = await secureGet(unique1URL)
+      responseUnique2 = await secureGet(unique2URL)
+      responseOverride = await secureGet(overrideURL)
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
     }
+
+    t.equal(responseIndex.statusCode, 200, 'requestIndex succeeds')
+    t.equal(responseUnique1.statusCode, 200, 'requestUnique1 succeeds')
+    t.equal(responseUnique2.statusCode, 200, 'requestUnique2 succeeds')
+    t.equal(responseOverride.statusCode, 200, 'requestOverride succeeds')
+
+    t.equal(responseIndex.body.replace(/\s/g, ''), fs.readFileSync(path.join(__dirname, 'site', 'index.html'), 'utf-8').replace(/\s/g, ''), 'site index overrides archive indices')
+    t.equal(responseUnique1.body, archive1UniqueContent, 'archive 1 unique content loads')
+    t.equal(responseUnique2.body, archive2UniqueContent, 'archive 2 unique content loads')
+    t.equal(responseOverride.body, archive2OverrideContent, 'archive 2 content overrides archive 1 content')
+
+    server.close()
+    t.end()
+
+    // Clean up
+    fs.unlinkSync(archive1Index)
+    fs.unlinkSync(archive1Unique)
+    fs.unlinkSync(archive1Override)
+    fs.unlinkSync(archive2Index)
+    fs.unlinkSync(archive2Unique)
+    fs.unlinkSync(archive2Override)
+    fs.rmdirSync(archive1)
+    fs.rmdirSync(archive2)
   })
 })
 
@@ -123,7 +131,7 @@ test('[site.js] 4042302', t => {
   const _4042302FilePath = path.join(__dirname, 'site', '4042302')
   fs.writeFileSync(_4042302FilePath, 'https://my-previous.site', 'utf-8')
 
-  const server = site.serve({path: 'test/site', callback: async () => {
+  const server = new Site({path: 'test/site'}).serve(async () => {
     let response
     try {
       response = await secureGet('https://localhost/this-page-exists-on-my-previous-site')
@@ -138,80 +146,79 @@ test('[site.js] 4042302', t => {
     fs.unlinkSync(_4042302FilePath)
     server.close()
     t.end()
-  }})
+  })
 })
 
 
 test('[site.js] serve method default 404 and 500 responses', t => {
+  //
+  // Test the default 404 and 500 responses of the serve method.
+  //
+  // We rename the folders of the custom messages so that they are not used
+  // and we rename them back once we’re done.
+  //
+
+  t.plan(4)
+
+  const custom404Folder = path.join(__dirname, 'site', '404')
+  const backup404Folder = path.join(__dirname, 'site', 'backup-404')
+
+  const custom500Folder = path.join(__dirname, 'site', '500')
+  const backup500Folder = path.join(__dirname, 'site', 'backup-500')
+
+  fs.renameSync(custom404Folder, backup404Folder)
+  fs.renameSync(custom500Folder, backup500Folder)
+
+  const server = new Site({path: 'test/site'}).serve(async () => {
+
+    // The server is initialised with the default messages. We can now
+    // rename the folders back.
+    fs.renameSync(backup404Folder, custom404Folder)
+    fs.renameSync(backup500Folder, custom500Folder)
+
     //
-    // Test the default 404 and 500 responses of the serve method.
+    // Test default 404 error.
     //
-    // We rename the folders of the custom messages so that they are not used
-    // and we rename them back once we’re done.
-    //
-
-    t.plan(4)
-
-    const custom404Folder = path.join(__dirname, 'site', '404')
-    const backup404Folder = path.join(__dirname, 'site', 'backup-404')
-
-    const custom500Folder = path.join(__dirname, 'site', '500')
-    const backup500Folder = path.join(__dirname, 'site', 'backup-500')
-
-    fs.renameSync(custom404Folder, backup404Folder)
-    fs.renameSync(custom500Folder, backup500Folder)
-
-    const server = site.serve({path: 'test/site', callback: async () => {
-
-      // The server is initialised with the default messages. We can now
-      // rename the folders back.
-      fs.renameSync(backup404Folder, custom404Folder)
-      fs.renameSync(backup500Folder, custom500Folder)
-
-      //
-      // Test default 404 error.
-      //
-      let responseDefault404
-      try {
-        responseDefault404 = await secureGet('https://localhost/this-page-does-not-exist')
-      } catch (error) {
-        console.log(error)
-        process.exit(1)
-      }
-
-      const expectedDefault404ResponseBodyDeflated = '<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error 404: Not found</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><h1 style="font-size: 16vw; color: black; text-align:center; line-height: 0.25">4🤭4</h1><p style="font-size: 4vw; text-align: center; padding-left: 2vw; padding-right: 2vw;"><span>Could not find</span> <span style="color: grey;">/this-page-does-not-exist</span></p></main></body></html>'.replace(/\s/g, '')
-
-      t.equal(responseDefault404.statusCode, 404, 'response status code is 404')
-      t.equal(responseDefault404.body.replace(/\s/g, ''), expectedDefault404ResponseBodyDeflated, 'default 404 response body is as expected')
-
-      //
-      // Test default 500 error.
-      //
-
-      let responseDefault500
-      try {
-        responseDefault500 = await secureGet('https://localhost/test-500-error')
-      } catch (error) {
-        console.log(error)
-        process.exit(1)
-      }
-
-      const expectedDefault500ResponseBodyDeflated = '<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error 500: Internal Server Error</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><h1 style="font-size: 16vw; color: black; text-align:center; line-height: 0.25">5🔥😱</h1><p style="font-size: 4vw; text-align: center; padding-left: 2vw; padding-right: 2vw;"><span>Internal Server Error</span><br><br><span style="color: grey;">Bad things have happened.</span></p></main></body></html>'.replace(/\s/g, '')
-
-      t.equal(responseDefault500.statusCode, 500, 'response status code is 500')
-      t.equal(responseDefault500.body.replace(/\s/g, ''), expectedDefault500ResponseBodyDeflated, 'default 500 response body is as expected')
-
-      t.end()
-
-      server.close()
+    let responseDefault404
+    try {
+      responseDefault404 = await secureGet('https://localhost/this-page-does-not-exist')
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
     }
+
+    const expectedDefault404ResponseBodyDeflated = '<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error 404: Not found</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><h1 style="font-size: 16vw; color: black; text-align:center; line-height: 0.25">4🤭4</h1><p style="font-size: 4vw; text-align: center; padding-left: 2vw; padding-right: 2vw;"><span>Could not find</span> <span style="color: grey;">/this-page-does-not-exist</span></p></main></body></html>'.replace(/\s/g, '')
+
+    t.equal(responseDefault404.statusCode, 404, 'response status code is 404')
+    t.equal(responseDefault404.body.replace(/\s/g, ''), expectedDefault404ResponseBodyDeflated, 'default 404 response body is as expected')
+
+    //
+    // Test default 500 error.
+    //
+
+    let responseDefault500
+    try {
+      responseDefault500 = await secureGet('https://localhost/test-500-error')
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
+    }
+
+    const expectedDefault500ResponseBodyDeflated = '<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Error 500: Internal Server Error</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><h1 style="font-size: 16vw; color: black; text-align:center; line-height: 0.25">5🔥😱</h1><p style="font-size: 4vw; text-align: center; padding-left: 2vw; padding-right: 2vw;"><span>Internal Server Error</span><br><br><span style="color: grey;">Bad things have happened.</span></p></main></body></html>'.replace(/\s/g, '')
+
+    t.equal(responseDefault500.statusCode, 500, 'response status code is 500')
+    t.equal(responseDefault500.body.replace(/\s/g, ''), expectedDefault500ResponseBodyDeflated, 'default 500 response body is as expected')
+
+    t.end()
+
+    server.close()
   })
 })
 
 
 test('[site.js] serve method', t => {
   t.plan(7)
-  const server = site.serve({path: 'test/site', callback: async () => {
+  const server = new Site({path: 'test/site'}).serve(async () => {
 
     t.ok(server instanceof https.Server, 'is https.Server')
 
@@ -268,5 +275,5 @@ test('[site.js] serve method', t => {
     t.end()
 
     server.close()
-  }})
+  })
 })
