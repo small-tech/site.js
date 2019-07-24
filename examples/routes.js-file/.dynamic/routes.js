bf0ed5910173854e329eea7249bba9cbc483fb63
Aral Balkan <mail@ar.al>
2019-07-24 18:39:48 +0100

    Minor: update comment

--- examples/routes.js-file/.dynamic/routes.js
+++ examples/routes.js-file/.dynamic/routes.js
@@ -12,7 +12,7 @@
 module.exports = app => {
 
   //
-  // An example of complex routing using a parameter. (This isn’t possible with dotJS routing.)
+  // An basic example of complex routing using a parameter. (This isn’t possible with dotJS routing.)
   //
   // For more examples, including the use of regular expressions in routes, please refer to
   // the Express Routing section of the ExpressJS documentation at
