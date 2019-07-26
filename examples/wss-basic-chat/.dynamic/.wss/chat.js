f44ad477255e33654791987eec28e076f3643c50
Aral Balkan <mail@ar.al>
2019-07-26 14:25:34 +0100

    Add broadcast ability to DotJS WebSocket routes and add chat example

---
+++ examples/wss-basic-chat/.dynamic/.wss/chat.js
@@ -0,0 +1,18 @@
+// Basic chat.
+//
+// Note: in order to have access to `this`, you must
+// ===== define your route using the function keyword
+//       – you cannot use an arrow function.
+//
+//       You can, however, use arrow functions within
+//       any inner functions inside of your route
+//       (like the event handler in this example).
+//
+module.exports = function (webSocket, request) {
+  webSocket.on('message', message => {
+    console.log('message', message)
+    this.getWss('/chat').clients.forEach(client => {
+      client.send(message)
+    })
+  })
+}
