58e1d04172a0a132bf56312503dcbaa5ab6e5419
Aral Balkan <aral@ind.ie>
2019-09-29 11:13:34 +0100

    Add mime type to dynamic cows example
    (On macOS, a missing mime type results in the content being downloaded as a .dms file.)

--- test/site/.dynamic/cows.js
+++ test/site/.dynamic/cows.js
@@ -6,7 +6,22 @@ function paveTheCowPaths(request, response, next) {
   const randomCowIndex = Math.round(Math.random()*cows.length)-1
   const randomCow = cows[randomCowIndex]
 
-  response.end(`<!doctype html><html lang="en" style="font-family: sans-serif; background-color: #eae7e1"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Cows!</title></head><body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;"><main><pre style="font-size: 24px;">${randomCow}</pre></main></body></html>`)
+  response
+    .type('html')
+    .end(`
+      <!doctype html>
+      <html lang="en" style="font-family: sans-serif; background-color: #eae7e1">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Cows!</title>
+        </head>
+        <body style="display: grid; align-items: center; justify-content: center; height: 100vh; vertical-align: top; margin: 0;">
+          <main>
+            <pre style="font-size: 24px;">${randomCow}</pre>
+          </main>
+        </body>
+      </html>`)
 }
 
 module.exports = paveTheCowPaths
