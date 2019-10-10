113d6b0f288f239387d47e5ca09a19cf5961301a
Aral Balkan <mail@ar.al>
2019-10-10 16:16:31 +0100

    Update basic chat example to match the one in the upcoming tutorial

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
   - Nothing yet
 
+## [12.8.0]- 2019-10-10
+
+### Changed
+
+  - Updated the chat example to match the one in the upcoming [Build a simple chat app with Site.js tutorial](https://localhost/2019/10/10/build-a-simple-chat-app-with-site.js).
+
 ## [12.7.0] - 2019-10-05
 
 ### Added
--- examples/wss-basic-chat/.dynamic/.wss/chat.js
+++ examples/wss-basic-chat/.dynamic/.wss/chat.js
@@ -1,31 +1,49 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Basic WebSocket Chat.
+// Basic chat app with Site.js (server).
 //
-// Note: in order to have access to `this`, you must define your route using the function keyword.
-// ===== You cannot use an arrow function. You can, however, use arrow functions within any inner
-//       functions inside of your route (like the event handler in this example).
+// For a step-by-step tutorial of how to build this example, see:
+// https://ar.al/2019/10/10/build-a-simple-chat-app-with-site.js
+//
+// Copyright ⓒ 2019 Aral Balkan.
+// Released under GNU AGPL version 3.0 or later.
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 module.exports = function (client, request) {
-  //
-  // A new client connection has been made.
-  //
-  // Persist the client’s room based on the path in the request.
-  //
+  // New client connection: persist client’s “room”
+  // based on request path.
   client.room = this.setRoom(request)
 
+  // Log the connection.
   console.log(`New client connected to ${client.room}`)
 
   client.on('message', message => {
-    //
-    // A new message has been received from a client.
-    //
-    // Broadcast it to every other client in the same room.
-    //
+    // New message received: broadcast it to all other clients
+    // in the same room after performing basic validation.
+    if (!isValidMessage(JSON.parse(message))) {
+      console.log(`Message is invalid; not broadcasting.`)
+      return
+    }
+
     const numberOfRecipients = this.broadcast(client, message)
 
-    console.log(`${client.room} message broadcast to ${numberOfRecipients} recipient${numberOfRecipients === 1 ? '' : 's'}.`)
+    // Log the number of recipients message was sent to
+    // and make sure we pluralise the log message properly.
+    console.log(`${client.room} message broadcast to `
+      + `${numberOfRecipients} recipient`
+      + `${numberOfRecipients === 1 ? '' : 's'}`)
   })
+}
+
+// Is the passed object a valid string?
+function isValidString(s) {
+  return Boolean(s)                // Isn’t null, undefined, '', or 0
+    && typeof s === 'string'       // and is the correct type
+    && s.replace(/\s/g, '') !== '' // and is not just whitespace.
+}
+
+// Is the passed message object valid?
+function isValidMessage(m) {
+  return isValidString(m.nickname) && isValidString(m.text)
 }
