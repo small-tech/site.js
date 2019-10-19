88b81578f385ccac2eb2d94c645cf5a9d99091c7
Aral Balkan <mail@ar.al>
2019-10-19 11:08:07 +0100

    WIP: Update command

--- bin/commands/update.js
+++ bin/commands/update.js
@@ -7,6 +7,7 @@
 //////////////////////////////////////////////////////////////////////
 
 const https = require('https')
+const os = require('os')
 const Site = require('../../index')
 
 async function secureGet (url) {
@@ -30,42 +31,70 @@ async function secureGet (url) {
 async function update () {
   Site.logAppNameAndVersion()
 
+  console.log(' 🧐 Checking for updates…\n')
+
   let response
   try {
     response = await secureGet('https://sitejs.org/version')
   } catch (error) {
-    console.log(`\n 🤯 Error: Could not check for updates.\n`)
+    console.log(' 🤯 Error: Could not check for updates.\n')
     console.log(error)
     process.exit(1)
   }
 
-  const latestVersion = '12.9.0' //response.body
+  const latestVersion = response.body
   const [latestMajor, latestMinor, latestPatch] = latestVersion.split('.')
 
-  const currentVersion = Site.versionNumber()
+  const currentVersion = '12.7.9' // Site.versionNumber()
   const [currentMajor, currentMinor, currentPatch] = currentVersion.split('.')
 
-  console.log(`Latest version: ${latestVersion} ${latestMajor} ${latestMinor} ${latestPatch}`)
-  console.log(`Current version: ${currentVersion} ${currentMajor} ${currentMinor} ${currentPatch}`)
+  // Debug.
+  // console.log(`Latest version: ${latestVersion} ${latestMajor} ${latestMinor} ${latestPatch}`)
+  // console.log(`Current version: ${currentVersion} ${currentMajor} ${currentMinor} ${currentPatch}`)
 
   if (currentVersion !== latestVersion) {
     // Are we running a newer (development or beta) version than the latest release version?
     if (currentMajor > latestMajor || (currentMajor === latestMajor && currentMinor > latestMinor) || (currentMajor === latestMajor && currentMinor === latestMinor && currentPatch > latestPatch)) {
-      console.log(`\n 🤓 You are running a newer version (${currentVersion}) than the latest released version (${latestVersion}). You geek, you!\n`)
+      console.log(` 🤓 You are running a newer version (${currentVersion}) than the latest released version (${latestVersion}).\n`)
       process.exit()
     }
 
     // The current version is not newer than the latest version and we know
     // that it isn’t equal to the release version so it must be older. Let’s
     // update!
-    console.log(`\n 🎁 There is a new version of Site.js available (${latestVersion})`)
+    console.log(` 🎁 There is a new version of Site.js available (v${latestVersion}).\n`)
 
-    // Get the right binary for the platform and architecture.
+    //
+    // Compose the right binary URL for the platform and architecture.
+    //
+    const platform = os.platform()
+    const cpuArchitecture = os.arch()
+
+    let platformPath = {
+      'linux': 'linux',
+      'darwin': 'macos',
+      'win32': 'windows'
+    }[platform]
+
+    if (platformPath === 'linux' && cpuArchitecture === 'arm') {
+      platform = `${platformPath}-arm`
+    }
+
+    let binaryUrl = `https://sitejs.org/releases/${platformPath}/${latestVersion}.tar.gz`
+
+    console.log(` 📡 Downloading Site.js version ${latestVersion}…`)
+
     // TODO
 
-    console.log(' ⏬ Downloading Site.js ')
+    console.log(' 📦 Installing…')
+
+    // TODO
+
+    console.log(' 🎉 Done!\n')
+
+    // TODO
   } else {
-    console.log('\n 😁👍 You’re running the latest version of Site.js!\n')
+    console.log(' 😁👍 You’re running the latest version of Site.js!\n')
   }
 }
 
