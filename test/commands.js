a68bb4180ea21b2fb7b99b5abfe68bf2af65406c
Aral Balkan <mail@ar.al>
2020-02-19 09:44:45 +0100

    Fix version command test to use the current (not installed) binary

--- test/commands.js
+++ test/commands.js
@@ -6,13 +6,13 @@ test('[bin/commands] version', t => {
 
   const version = require('../package.json').version
 
-  const expectedOutputConcentrate = `💕Site.jsv${version}(runningonNodev10.16.0)╔═══════════════════════════════════════════╗║Likethis?Fundus!║║║║We’reatiny,independentnot-for-profit.║║https://small-tech.org/fund-us║╚═══════════════════════════════════════════╝`
+  const expectedOutputConcentrate = `💕Site.jsv${version}(runningonNode${process.version})╔═══════════════════════════════════════════╗║Likethis?Fundus!║║║║We’reatiny,independentnot-for-profit.║║https://small-tech.org/fund-us║╚═══════════════════════════════════════════╝`
 
   // Ensure that the command logs to console (as tests are being run with QUIET=true in the environment.)
   let env = Object.assign({}  , process.env)
   delete env['QUIET']
 
-  const output = childProcess.execSync('site version', { env })
+  const output = childProcess.execSync('node bin/site.js version', { env })
 
   t.strictEquals(output.toString('utf-8').replace(/\s/g,''), expectedOutputConcentrate, 'Command output is as expected')
   t.end()
