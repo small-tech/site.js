0205a44ced007c6905ee7540b9e622d66bded612
Aral Balkan <mail@ar.al>
2020-06-19 14:18:05 +0100

    Closes #145: update logs command to show more of the latest logs

--- bin/commands/logs.js
+++ bin/commands/logs.js
@@ -16,7 +16,7 @@ function logs () {
   Site.logAppNameAndVersion()
   ensure.journalctl()
   console.log(`   📜    ❨site.js❩ Tailing logs (press Ctrl+C to exit).\n`)
-  childProcess.spawn('journalctl', ['--follow', '--unit', 'site.js'], {env: process.env, stdio: 'inherit'})
+  childProcess.spawn('journalctl', ['--since', 'today', '--no-pager', '--follow', '--unit', 'site.js'], {env: process.env, stdio: 'inherit'})
 }
 
 module.exports = logs
