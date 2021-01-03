160379ef74ea498fee18904ee0ece2bb41759c32
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-01-03 12:32:12 +0000

    Fixes #258: crash on enable on systems without Node.js installed

--- bin/commands/enable.js
+++ bin/commands/enable.js
@@ -76,8 +76,7 @@ function enable (args) {
       const _pathToServe = args.positional.length === 1 ? args.positional[0] : '.'
       const binaryExecutable = '/usr/local/bin/site'
       const sourceDirectory = path.resolve(__dirname, '..', '..')
-      const nodeExecutable = `${childProcess.execSync('which node').toString().trim()} ${path.join(sourceDirectory, 'bin/site.js')}`
-      const executable = runtime.isBinary ? binaryExecutable : nodeExecutable
+      const executable = runtime.isBinary ? binaryExecutable : `${childProcess.execSync('which node').toString().trim()} ${path.join(sourceDirectory, 'bin/site.js')}`
 
       // It is a common mistake to start the server in a .dynamic folder (or subfolder)
       // or a .hugo folder or subfolder. In these cases, try to recover and do the right thing.
