12d5674f1f470cac072ab956f5c1adb08cd9d9ca
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-14 16:21:17 +0100

    Start command now also starts Owncast if necessary

--- bin/lib/start.js
+++ bin/lib/start.js
@@ -6,6 +6,8 @@
 //
 //////////////////////////////////////////////////////////////////////
 
+const fs = require('fs')
+const path = require('path')
 const childProcess = require('child_process')
 const status = require('../lib/status')
 const clr = require('../../lib/clr')
@@ -34,6 +36,19 @@ function start () {
     childProcess.execSync('sudo systemctl start site.js', {env: process.env, stdio: 'pipe'})
   } catch (error) {
     throwError(`Could not start Site.js server (${error}).`)
+  }
+
+  // Also see if we should start the Owncast service.
+  const systemdServicesDirectory = path.join('/', 'etc', 'systemd', 'system')
+  const owncastServiceFilePath = path.join(systemdServicesDirectory, 'owncast.service')
+  if (fs.existsSync(owncastServiceFilePath)) {
+    console.log('   💮️    ❨site.js❩ Also starting Owncast service.')
+    try {
+      // Start the Owncast service.
+      childProcess.execSync('sudo systemctl start owncast', {env: process.env, stdio: 'pipe'})
+    } catch (error) {
+      throwError(`Could not start Owncast service (${error}).`)
+    }
   }
 
   console.log('\n   🎈    ❨site.js❩ Server started.\n')
